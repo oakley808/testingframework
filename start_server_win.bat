9d98803b005fc1953b4a731f2c49bf44e52c2b42
Nicholas Rosevear <nrosevear@homeadvisor.com>
2016-09-28 14:26:45 -0600

    updated start server script

--- start_server_win.bat
+++ start_server_win.bat
@@ -1,4 +1,4 @@
-set base=%~dp0drivers\win\chromedriver.exe
+set base=%~dp0node_modules\chromedriver\lib\chromedriver\chromedriver.exe
 echo %base%
 echo starting server java -jar drivers/selenium-standalone.jar -Dwebdriver.chrome.driver=%base%
 java -jar drivers\selenium-server-standalone.jar -Dwebdriver.chrome.driver=%base%
